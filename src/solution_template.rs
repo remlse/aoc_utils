07f7af914568f9f9be319aac3c4ae3bc86cd1d2b
Remo Senekowitsch <contact@remlse.dev>
2023-05-13 21:32:49 +0200

    add ability to ignore test in solution macro

--- src/solution_template.rs
+++ src/solution_template.rs
@@ -39,4 +39,46 @@ macro_rules! solution {
             assert_eq!($package::part2(INPUT), $res_part2);
         }
     };
+
+    // variants for ignoring
+    // ignore part1
+    ($package:ident; ignore $res_part1:expr; $res_part2:expr;) => {
+        static INPUT: &str = include_str!("../input/input.txt");
+        #[test]
+        #[ignore]
+        fn test_part1() {
+            assert_eq!($package::part1(INPUT), $res_part1);
+        }
+        #[test]
+        fn test_part2() {
+            assert_eq!($package::part2(INPUT), $res_part2);
+        }
+    };
+    // ignore part2
+    ($package:ident; $res_part1:expr; ignore $res_part2:expr;) => {
+        static INPUT: &str = include_str!("../input/input.txt");
+        #[test]
+        fn test_part1() {
+            assert_eq!($package::part1(INPUT), $res_part1);
+        }
+        #[test]
+        #[ignore]
+        fn test_part2() {
+            assert_eq!($package::part2(INPUT), $res_part2);
+        }
+    };
+    // ignore both parts
+    ($package:ident; ignore $res_part1:expr; ignore $res_part2:expr;) => {
+        static INPUT: &str = include_str!("../input/input.txt");
+        #[test]
+        #[ignore]
+        fn test_part1() {
+            assert_eq!($package::part1(INPUT), $res_part1);
+        }
+        #[test]
+        #[ignore]
+        fn test_part2() {
+            assert_eq!($package::part2(INPUT), $res_part2);
+        }
+    };
 }
